829be4eed391154c71fa591477186ff4541bcd66
Daniel Villahermosa <daniel.villahermosa@codurance.com>
2023-08-03 16:13:08 +0200

    case V

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -3,6 +3,9 @@ package org.example;
 public class RomanNumerals {
     public String process(int number){
         String result = "";
+        if(number == 5) {
+            return "V";
+        }
         for(int i = 0; i < number; i++){
             result += "I";
         }
--- src/test/java/org/example/RomanNumeralsShould.java
+++ src/test/java/org/example/RomanNumeralsShould.java
@@ -1,11 +1,9 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RomanNumeralsShould {
 
@@ -14,11 +12,13 @@ public class RomanNumeralsShould {
             "0,''",
             "1,I",
             "2,II",
-            "3,III"
+            "3,III",
+            "5,V",
     })
     void convert_number_to_a_i_roman_number(int number, String expected) {
         RomanNumerals romanNumerals = new RomanNumerals();
         String result = romanNumerals.process(number);
         assertEquals(expected, result);
     }
+
 }
