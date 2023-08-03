7569ad97c731776e3e64780922db9382dc758749
Daniel Villahermosa <daniel.villahermosa@codurance.com>
2023-08-03 15:25:35 +0200

    first test

---
+++ src/test/java/org/example/FooShould.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FooShould {
+    @Test
+    void foo() {
+        assertTrue(true);
+    }
+}
